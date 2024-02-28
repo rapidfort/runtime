ed73a5e98877c557a32c2b3fa6e3f7f688b5add6
Vinod Gupta <vinod@rapidfort.com>
2024-02-27 17:50:26 -0800

    make cnab bundling work

--- cnab_publish.sh
+++ cnab_publish.sh
@@ -3,5 +3,6 @@
 az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
 az acr login -n $REGISTRY_NAME
 cd /data/
+ls -l
 cpa verify
 cpa buildbundle
